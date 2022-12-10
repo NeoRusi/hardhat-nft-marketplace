b4eb81d35b50e0d8173f2a9e70a971964c9941cd
Ruslan <103575933+NeoRusi@users.noreply.github.com>
2022-12-10 16:52:30 +0300

    Add files via upload

--- scripts/mint-and-list.js
+++ scripts/mint-and-list.js
@@ -1,5 +1,5 @@
 const { ethers, network } = require("hardhat")
-const { moveBlocks } = requird("../utils/move-blocks")
+const { moveBlocks } = require("../utils/move-blocks")
 
 const PRICE = ethers.utils.parseEther("0.1")
 
